ed21806281bdab73f73478739219a625c88b7c64
Cesar <50643126+cesar-yoab@users.noreply.github.com>
2023-04-23 23:42:00 +0000

    Updated readme and started working on fault tolerant broadcast challenge

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Gossip Gloomers
-Solutions to Gossip Gloomer challenges a set of distributed systems challenges.
+Solutions to [Gossip Gloomer](https://fly.io/dist-sys/) challenges a set of distributed systems challenges.
 
 1. Echo Challenge
 2. Unique ID Generator
@@ -16,10 +16,39 @@ All messages are sent in json format with the following structure:
 {
   "src": "c1",
   "dest": "n1",
-  "body": {
-    "type": "echo",
-    "msg_id": 1,
-    "echo": "Please echo 35"
-  }
+  "body": <json_body>
 }
 ```
+
+## Echo Challenge
+More of a "getting started" guide" to get the hang of working with Maelstrom in Go. 
+
+## Unique ID Generator
+In this challenge we need to implement a globally-unique ID generation system that runs against Maelstrom's unique-ids workload. The service should be totally available, meaning that it can continue to operate even in the face of network partitions.
+
+## Broadcast
+On this challenge, we need to implement a broadcast system that gossips messages between all nodes in the cluster. Gossiping is a common way to propagate information across a cluster when you don't need strong consistency guarantees.
+
+This challenge is broken up in multiple sections so that you can build out your system incrementally.
+
+### Single-Node Broadcast
+First, we start out with a single-node broadcast system. That may sound like an oxymoron but this lets us get our message handlers working correctly in isolation before trying to share messages between nodes.
+
+### Multi-Node Broadcast
+In this challenge, we build on our Single-Node Broadcast implementation and replicate our messages across a cluster that has no network partitions.
+
+The node should propagate values it sees from broadcast messages to the other nodes in the cluster. It can use the topology passed to your node in the topology message or you can build your own topology.
+
+The simplest approach is to simply send a node's entire data set on every message, however, this is not practical in a real-world system. Instead, try to send data more efficiently as if you were building a real broadcast system.
+
+Values should propagate to all other nodes within a few seconds.
+
+### Fault Tolerant Broadcast
+In this challenge, we build on our Multi-Node Broadcast implementation, however, this time we'll introduce network partitions between nodes so they will not be able to communicate for periods of time.
+
+The node should propagate values it sees from broadcast messages to the other nodes in the cluster—even in the face of network partitions! Values should propagate to all other nodes by the end of the test. Nodes should only return copies of their own local values.
+
+
+### Efficient Broadcast Pt. 1
+
+### Efficient Broadcast Pt. 2
---
+++ fault-tolerant-broadcast/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+type Topology struct {
+	Type     string              `json:"type,omitempty`
+	Topology map[string][]string `json:"topology,omitempty`
+}
+
+func PushMessage(n *maelstrom.Node, initNode string, lastNode string, node any, message any) error {
+	dest, ok := node.(string)
+	if !ok {
+		return fmt.Errorf("message is not a string")
+	}
+	body := map[string]any{"init_node": initNode, "message": message, "type": "push", "last_node": lastNode}
+	return n.Send(dest, body)
+}
+
+func InStore(store *[]any, value any) bool {
+	for _, v := range *store {
+		if v == value {
+			return true
+		}
+	}
+
+	return false
+}
+
+func main() {
+	n := maelstrom.NewNode()
+	store := make([]any, 0)
+	messageStore := make(map[any]bool)
+	var topology Topology
+
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		// Unmarshall the message body as an loosely-typed map.
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		// save message to be read later
+		message := body["message"]
+		// store = append(store, message)
+		messageStore[message] = true
+		delete(body, "message")
+
+		// update message type to ok
+		body["type"] = "broadcast_ok"
+		initNode := msg.Dest
+
+		if topology.Topology != nil {
+			for _, node := range topology.Topology[initNode] {
+				if err := PushMessage(n, initNode, initNode, node, message); err != nil {
+					return err
+				}
+			}
+		}
+
+		// Echo the original message back with the update message type.
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("push", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		message := body["message"]
+		if inStore := InStore(&store, message); !inStore {
+			store = append(store, message)
+		}
+
+		initNode, ok := body["init_node"].(string)
+		if !ok {
+			return fmt.Errorf("init_node is not a string")
+		}
+		lastNode, ok := body["last_node"].(string)
+		if !ok {
+			return fmt.Errorf("last_node is not a string")
+		}
+
+		self := msg.Dest
+		if topology.Topology != nil {
+			for _, node := range topology.Topology[self] {
+				if node == initNode || node == lastNode {
+					continue
+				}
+
+				if err := PushMessage(n, initNode, self, node, message); err != nil {
+					return err
+				}
+			}
+		}
+
+		return nil
+	})
+
+	n.Handle("read", func(msg maelstrom.Message) error {
+		// Unmarshall the message body as an loosely-typed map.
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		// Update the message type to return back
+
+		body["type"] = "read_ok"
+		body["messages"] = store
+
+		// Echo the original message back with the update message type.
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		// Unmarshall the topology map and return.
+		if err := json.Unmarshal(msg.Body, &topology); err != nil {
+			return err
+		}
+
+		// Update the message type to return back
+		body := map[string]string{"type": "topology_ok"}
+		delete(body, "topology")
+		// Echo the original message back with the update message type.
+		return n.Reply(msg, body)
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
